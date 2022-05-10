5dd46cd9d978d8efd4ebbd3afb251e67e14264b3
hjun-park <phj0860@gmail.com>
2022-05-10 17:54:02 +0900

    Init: 프로젝트 생성 및 build.gradle 세팅

---
+++ src/test/java/com/hansol/hrm/HrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.hrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
