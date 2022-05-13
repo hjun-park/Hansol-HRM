74bc760e248cf3878ef47fcb4413914fdda2fa72
hjun-park <phj0860@gmail.com>
2022-05-13 10:29:27 +0900

    Test: 직원관리 테스트코드 추가 및 Lifecycle 변경

--- src/test/java/com/hansol/hrm/service/staff/StaffServiceImplTest.java
+++ src/test/java/com/hansol/hrm/service/staff/StaffServiceImplTest.java
@@ -1,17 +1,13 @@
 package com.hansol.hrm.service.staff;
 
-import static com.hansol.hrm.global.response.BaseResponseStatus.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hansol.hrm.global.exception.BaseException;
-import com.hansol.hrm.global.response.BaseResponseStatus;
-import com.hansol.hrm.service.staff.domain.StaffEntity;
+import com.hansol.hrm.service.staff.domain.Staff;
 import com.hansol.hrm.service.staff.domain.StaffMapper;
 import com.hansol.hrm.service.staff.dto.StaffDto;
 
@@ -45,8 +40,8 @@ class StaffServiceImplTest {
 		//given
 
 		//when
-		List<StaffEntity> staffEntities = staffMapper.findAll();
-		List<StaffDto> staffDtoList = staffService.findStaffs().orElseGet(Collections::emptyList);
+		List<Staff> staffEntities = staffMapper.findAll();
+		List<StaffDto> staffDtoList = staffService.findStaffs();
 
 		//then
 		assertEquals(staffEntities.size(), staffDtoList.size());
@@ -54,66 +49,126 @@ class StaffServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("02. 직원 추가 테스트")
+	@DisplayName("01-1. 존재하지 않는 ID로 조회")
 	@Order(2)
-	void addStaff() {
+	void findStaffByNotExistsId() {
 		//given
 		StaffDto staffDto = StaffDto.builder()
 			.name("박현준")
 			.type("인턴")
-			.phone_number("010-1111-2222")
+			.phoneNumber("010-1111-2222")
 			.positionId(5L)
 			.taskId(16L)
 			.build();
 
 		//when
-		staffService.addStaff(staffDto);
+		Long staffId = staffService.addStaff(staffDto);
 
 		//then
-		assertEquals(staffMapper.findByName("박현준").getName(), "박현준");
+		assertThrows(BaseException.class, () -> staffService.findStaffById(staffId + 1L));
+
+	}
+
+	@Test
+	@DisplayName("01-2. 특정 이름 가진 직원 조회")
+	@Order(3)
+	void findStaffByName() {
+		//given
+		StaffDto staffDto1 = StaffDto.builder()
+			.name("박현준")
+			.type("인턴")
+			.phoneNumber("010-1111-2222")
+			.positionId(5L)
+			.taskId(16L)
+			.build();
+
+		StaffDto staffDto2 = StaffDto.builder()
+			.name("김현준")
+			.type("인턴")
+			.phoneNumber("010-2222-3333")
+			.positionId(5L)
+			.taskId(16L)
+			.build();
+
+		staffService.addStaff(staffDto1);
+		staffService.addStaff(staffDto2);
+
+		//when
+
+		//then
+		assertEquals(staffMapper.findByName("현준").size(), staffService.findStaffsByName("현준").size());
+
+	}
+
+	@Test
+	@DisplayName("02. 직원 추가 테스트")
+	@Order(5)
+	void addStaff() {
+		//given
+		StaffDto staffDto = StaffDto.builder()
+			.name("박현준")
+			.type("인턴")
+			.phoneNumber("010-1111-2222")
+			.positionId(5L)
+			.taskId(16L)
+			.build();
+
+		//when
+		Long staffId = staffService.addStaff(staffDto);
+		StaffDto staffById = staffService.findStaffById(staffId);
+
+		//then
+		assertEquals(staffId, staffById.getId());
 
 	}
 
 	@Test
 	@DisplayName("03. 직원 수정 테스트")
-	@Order(3)
+	@Order(10)
 	void editStaff() {
 		//given
 		StaffDto staffDto = StaffDto.builder()
 			.name("박현준")
 			.type("인턴")
-			.phone_number("010-1111-2222")
+			.phoneNumber("010-1111-2222")
 			.positionId(5L)
 			.taskId(16L)
 			.build();
 
-		staffService.addStaff(staffDto);
-		StaffDto findDto = staffService.findStaffByName("박현준").orElseThrow(() -> new BaseException(TEST_FAIL));
+		Long staffId = staffService.addStaff(staffDto);
+		StaffDto findDto = staffService.findStaffById(staffId);
 
 		//when
 		findDto.setName("테스트유저123");
 		staffService.editStaff(findDto.getId(), findDto);
 
 		//then
-		assertEquals(staffMapper.findByName("테스트유저123").getName(), "테스트유저123");
+		Staff staffEntity = staffMapper.findById(staffId);
+
+		System.out.println("staffEntity = " + staffEntity.toString());
+		assertEquals(staffEntity.getName(), "테스트유저123");
+		assertEquals(staffEntity.getType(), "인턴");
+		assertEquals(staffEntity.getPhoneNumber(), "010-1111-2222");
+		assertEquals(staffEntity.getPositionId(), 5L);
+		assertEquals(staffEntity.getTaskId(), 16L);
 
 	}
 
 	@Test
 	@DisplayName("04. 직원 상태 삭제 변경 테스트")
-	@Order(4)
+	@Order(15)
 	void statusToDeleteStaff() {
 		//given
 		StaffDto staffDto = StaffDto.builder()
 			.name("박현준")
 			.type("인턴")
-			.phone_number("010-1111-2222")
+			.phoneNumber("010-1111-2222")
 			.positionId(5L)
 			.taskId(16L)
 			.build();
 
-		staffService.addStaff(staffDto);
-		StaffDto findDto = staffService.findStaffByName("박현준").orElseThrow(() -> new BaseException(TEST_FAIL));
+		Long staffId = staffService.addStaff(staffDto);
+		StaffDto findDto = staffService.findStaffById(staffId);
 
 		//when
 		staffService.statusToDeleteStaff(findDto.getId());
@@ -122,7 +177,4 @@ class StaffServiceImplTest {
 		assertNull(staffMapper.findById(findDto.getId()));
 
 	}
-
-	// TODO: null값에 대해 테스트 진행
-
 }
